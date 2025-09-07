e620bb2201d0fc05803ac78c029e04459c89e8db
GP_4465 <olusyamazurenco@gmail.com>
2025-09-07 14:09:55 +0300

    Create Setup.py

---
+++ Setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="filemenu",
+    version="1.0.0",
+    description="Консольное меню для работы с файлами и HTML",
+    author="Твоё имя",
+    packages=find_packages(),
+    python_requires=">=3.6",
+)
